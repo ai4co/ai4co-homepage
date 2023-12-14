14dae2723fe4a6be4290d6da73c1bbcd3d045d71
cbhua <cbhua@kaist.ac.kr>
2023-12-15 03:03:59 +0900

    [Feat] init research, blog, about pages

--- js/nav.js
+++ js/nav.js
@@ -4,3 +4,21 @@ document.addEventListener('DOMContentLoaded', function() {
         menuButton.classList.toggle('is-active');
     });
 });
+
+document.querySelector('.navbar-icon').addEventListener('click', function() {
+    document.querySelector('.navbar-menu').classList.toggle('show');
+});
+
+function copyToClipboard(element) {
+    var text = document.querySelector(element).innerText;
+    var elem = document.createElement("textarea");
+    document.body.appendChild(elem);
+    elem.value = text;
+    elem.select();
+    document.execCommand("copy");
+    document.body.removeChild(elem);
+  
+    // Optional: Show an alert or change the button text to give feedback
+    alert("Copied to clipboard");
+  }
+  
